c96e602340d8475f2aae11e1f4ea8a081d36a5a3
Douglas Miranda <dms07011@gmail.com>
2021-08-04 23:35:15 -0300

    ok

--- vuetify-components/moviecard.js
+++ vuetify-components/moviecard.js
@@ -6,24 +6,12 @@ Vue.component("vuetify-moviecard", {
     },
   },
   template: `
-  <v-card
-    class="mx-auto my-12"
-    max-width="374">
-    <template slot="progress">
-        <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-        ></v-progress-linear>
-    </template>
-
+  <v-card class="mt-12">
     <v-img
         height="250"
         v-bind:src='movie.img_url'>
     </v-img>
-
     <v-card-title>{{movie.original_title ? movie.original_title : movie.name}}</v-card-title>
-
     <v-card-text>
         <v-row
         align="center"
@@ -40,21 +28,17 @@ Vue.component("vuetify-moviecard", {
                 {{movie.vote_average/2}}
             </div>
         </v-row>
-
         <div class="my-4 text-subtitle-1">
             Linguagem: {{movie.original_language}}
         </div>
-
         <div>Lançado em: {{movie.release_date ? movie.release_date : movie.first_air_date}}</div>
     </v-card-text>
-
     <v-divider class="mx-4"></v-divider>
-
     <v-card-actions>
         <v-btn
-        color="deep-purple lighten-2"
-        text>
-        Reserve
+            color="deep-purple lighten-2"
+            text>
+            Reserve
         </v-btn>
     </v-card-actions>
 </v-card>
